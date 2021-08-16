a4994ee26a20010aefe0059cf240e3141471eb96
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-08-16 11:23:21 +0300

    install redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./redux/store";
 
 import "./styles.css";
+
+console.log(store);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
