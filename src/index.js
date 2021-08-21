ddf5680e0f11e1577ef718d95dbfb4defaf85262
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-08-21 16:46:33 +0300

    update package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
 
@@ -10,7 +11,9 @@ console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
